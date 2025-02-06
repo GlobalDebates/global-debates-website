174f058dd392f09d8271fcae9b9eda3fb8a71a6d
Ruman <ruman.leo@gmail.com>
2025-02-06 22:00:15 +0530

    fix: default theme set to system when toggling

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider attribute="class">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Navbar />
           <div>{children}</div>
         </ThemeProvider>
